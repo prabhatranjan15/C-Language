3477d731bb73d6c4e5b922498b82cb1ba9571a64
Prabhat ranjan <72509632+prabhatranjan15@users.noreply.github.com>
2021-01-23 19:56:24 +0530

    Create Playing With Characters.c

---
+++ Playing With Characters.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+#define MAX_LEN 128
+
+int main() 
+{
+ char ch;
+ char word[MAX_LEN];
+char sen[MAX_LEN];
+scanf("%c", &ch);
+ scanf("%s\n", &word);
+ scanf("%[^\n]%*c", &sen);
+ printf("%c\n", ch);
+ printf("%s\n", word);
+ printf("%s\n", sen);
+ return 0;
+         }
